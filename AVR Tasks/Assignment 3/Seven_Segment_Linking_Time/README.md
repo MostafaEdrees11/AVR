890ad1a05c4f38e94d547a0ba63b5a15c5c8329e
Mostafa Edrees <mostafaedrees018@gmail.com>
2023-10-25 22:27:30 +0300

    test Seven segment driver with two applications

---
+++ AVR Tasks/Assignment 3/Seven_Segment_Linking_Time/README.md
@@ -0,0 +1,6 @@
+# Seven Segment Driver.
+## Simulation On Proteus:
+### Count up from  0 to 99
+![gitHub]()
+### Presistance of Vision (POV) --> 25
+![gitHub]()
--- AVR Tasks/Assignment 3/Seven_Segment_Linking_Time/Seven_Segment_Linking_Time/main.c
+++ AVR Tasks/Assignment 3/Seven_Segment_Linking_Time/Seven_Segment_Linking_Time/main.c
@@ -1,9 +1,12 @@
-/*
- * Seven_Segment_Linking_Time.c
- *
- * Created: 10/20/2023 10:26:27 PM
- * Author : Mostafa Edrees
- */ 
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Mostafa Edrees
+ * @brief          : application file of Seven_Segment_Linking_Time.
+ * @date           : 20/13/2023
+ * @board          : ATMEGA32
+ ******************************************************************************
+**/
 #define F_CPU	1000000UL
 
 #include "stdTypes.h"
@@ -17,27 +20,51 @@
 
 extern SEG_t SEV_SEG_AstrSEGConfig[SEG_NUM];
 
+//POV to display 25
 int main(void)
 {
 	ES_t error;
 	error = Seven_Segment_enuInit(SEV_SEG_AstrSEGConfig);
 	
-    /* Replace with your application code */
-    while (1) 
-    {
+	/* Replace with your application code */
+	while (1)
+	{
 		if(error == ES_OK)
 		{
-			for(u8 i = 0; i < 10; i++)
-			{
-				Seven_Segment_enuDisplayNumber(0,i);
-				Seven_Segment_enuDisplayNumber(1,0);
-				for(u8 j = 0; j < 10; j++)
-				{
-					Seven_Segment_enuDisplayNumber(1,j);
-					_delay_ms(300);
-				}
-			}	
+			Seven_Segment_enuDisableCommon(1);
+			Seven_Segment_enuDisplayNumber(0,5);
+			Seven_Segment_enuEnableCommon(0);
+			_delay_ms(10);
+			Seven_Segment_enuDisableCommon(0);
+			Seven_Segment_enuDisplayNumber(1,2);
+			Seven_Segment_enuEnableCommon(1);
+			_delay_ms(15);
 		}
-    }
+	}
 }
+
+//count up from 0 to 99
+//int main(void)
+//{
+//	ES_t error;
+//	error = Seven_Segment_enuInit(SEV_SEG_AstrSEGConfig);
+//	
+//    /* Replace with your application code */
+//    while (1) 
+//    {
+//		if(error == ES_OK)
+//		{
+//			for(u8 i = 0; i < 10; i++)
+//			{
+//				Seven_Segment_enuDisplayNumber(0,i);
+//				Seven_Segment_enuDisplayNumber(1,0);
+//				for(u8 j = 0; j < 10; j++)
+//				{
+//					Seven_Segment_enuDisplayNumber(1,j);
+//					_delay_ms(300);
+//				}
+//			}	
+//		}
+//    }
+//}
 
